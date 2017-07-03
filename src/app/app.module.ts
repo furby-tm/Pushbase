8021df80203b5888c8202c40eb68320859fad09d
Tyler Furreboe <tylerfurby@Tylers-iMac.local>
2017-07-03 16:22:23 -0600

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NewsFeedComponent } from './news-feed.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsFeedComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/news-feed.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'feed-item',
+  templateUrl: 'news-feed.component.html',
+  styleUrls: ['news-feed.component.css']
+})
+
+export class NewsFeedComponent {
+
+}
